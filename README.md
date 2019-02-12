9876ceb3fc8c043d4cd0fa308c1c61f2b212f665
Prakash Pandey <prakashpandeyy@yahoo.com>
2019-02-13 00:16:35 +0530

    implemented binary string to string conversion

--- README.md
+++ README.md
@@ -12,7 +12,17 @@ I was having some free time and was in fun mood. I and my friend used it to chat
 
 ### How to use it?
 
-- `./binary -s="mad on the mars"`
+- **String to binary conversion**: 
+    
+    **command**: `./binary -s="mad on the mars"`
+    
+    **output**: `01101101 01100001 01100100 00100000 01101111 01101110 00100000 01110100 01101000 01100101 00100000 01101101 01100001 01110010 01110011`
+
+- **Binary string to string conversion**: 
+    
+    **command**: `./binary -b="01101101 01100001 01100100 00100000 01101111 01101110 00100000 01110100 01101000 01100101 00100000 01101101 01100001 01110010 01110011"`
+    
+    **output**: `mad on the mars` 
 
 [OPTIONAL] export it to your `PATH` for a convenient use
 
--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Flags struct {
-	input string
+	inputStr       string
+	inputBinaryStr string
 }
 
 func (f *Flags) Define() {
-	flag.StringVar(&f.input, "s", "", "enter input for string to 8-bit binary conversion")
+	flag.StringVar(&f.inputStr, "s", "", "enter input for string to 8-bit binary conversion")
+	flag.StringVar(&f.inputBinaryStr, "b", "", "enter input for to 8-bit binary to string conversion")
 }
 
 func (f *Flags) Parse() {
@@ -24,10 +28,29 @@ func stringToBinary(s string) (b string) {
 	return
 }
 
+func binaryToString(bytes string) (string, error) {
+	b := make([]byte, 0)
+	for _, field := range strings.Fields(bytes) {
+		v, err := strconv.ParseUint(field, 2, 8)
+		if err != nil {
+			return "", err
+		}
+		b = append(b, byte(v))
+	}
+	return string(b), nil
+}
+
 func main() {
 	f := Flags{}
 	f.Define()
 	f.Parse()
-	b := stringToBinary(f.input)
-	fmt.Printf("Original: %s\nBinary: %s\n", f.input, b)
+	var input, output string
+	if output = strings.TrimSpace(f.inputStr); output != "" {
+		input = f.inputStr
+		output = stringToBinary(f.inputStr)
+	} else if output = strings.TrimSpace(f.inputBinaryStr); output != "" {
+		input = f.inputBinaryStr
+		output, _ = binaryToString(f.inputBinaryStr)
+	}
+	fmt.Printf("Input: %s\nOutput: %s\n", input, output)
 }
